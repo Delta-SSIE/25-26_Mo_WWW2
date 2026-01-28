b342f9fedcba7c298a17577f4801eafff948c4e4
Jan Mottl <mottja@delta-skola.cz>
2026-01-28 08:21:05 +0100

    kk

---
+++ 2A/JS_Základ/funkce_a.js
@@ -0,0 +1,41 @@
+let x = 0;
+console.log(x);
+
+//ctrl + K; Q - zapnutí Quokka
+
+pozdrav();
+pozdrav();
+pozdrav();
+
+function pozdrav()
+{
+    console.log("Hello!");
+}
+
+function scitani(a,b)
+{
+    return a+b;
+}
+
+console.log(scitani(5,10));
+console.log(scitani(5));
+console.log(scitani());
+
+
+let a = 0;
+function plus()
+{
+    a++;
+}
+
+plus();
+plus();
+console.log(a);
+
+const nasobeni = function(a,b) { return a*b};
+
+console.log(nasobeni(5,5));
+
+const deleni = (a,b) => a/b;
+
+console.log(deleni(nasobeni(5,5),5));
