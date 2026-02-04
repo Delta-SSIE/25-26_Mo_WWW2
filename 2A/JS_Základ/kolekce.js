5ad4e714f0285096dd29ba52397ffb95a4595d7c
Jan Mottl <mottja@delta-skola.cz>
2026-02-04 08:20:20 +0100

    kk

--- 2A/JS_Základ/kolekce.js
+++ 2A/JS_Základ/kolekce.js
@@ -69,3 +69,48 @@ console.log(cislaNasobena);
 
 const poleFilter = cislaNasobena.filter((value) => value > 5);
 console.log(poleFilter);
+
+//Sude prvky
+const pole_prvky = [23, -8, 6, 3, 12, 2, 0, 0, 5, 18, 6, 14, 15];
+console.log(pole_prvky.filter((a) => a % 2 == 0));
+
+//Sety
+const set = new Set();
+const set2 = new Set([5, 10, 1]);
+
+set.add(10);
+set.add(10); //podruhé se nepřidá, protože už v setu je
+console.log(set);
+
+console.log(set2.has(1));
+
+for(let prvek of set2)
+{
+    console.log(prvek);
+}
+
+const dluhy = new Map();
+
+dluhy.set("Jaromír",500);
+dluhy.set("František",100);
+dluhy.set("Pepa",50);
+
+console.log(dluhy);
+console.log(dluhy.get("Pepa"));
+
+for(let klic of dluhy.keys())
+{
+    console.log(klic);
+}
+
+for(let value of dluhy.values())
+{
+    console.log(value);
+}
+
+for(let zaznam of dluhy.entries())
+{
+    console.log(zaznam);
+    console.log(zaznam[0]);
+    console.log(zaznam[1]);
+}
