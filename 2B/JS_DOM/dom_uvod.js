300ea74b1a3f81d275b078b5380384e0df180a9d
Jan Mottl <mottja@delta-skola.cz>
2026-02-24 11:15:11 +0100

    kk

---
+++ 2B/JS_DOM/dom_uvod.js
@@ -0,0 +1,37 @@
+//Document.write
+
+// document -> DOM
+// window -> BOM
+
+document.write("<h3>AAAAAAA!</h3>");
+
+
+//Pop-up (BOM)
+
+//Alert
+//window.alert("Pomoc! Prosím, zavolejte policii!");
+
+//Prompt
+
+/*
+let input = window.prompt("Zadej mi své jméno","Pepa");
+document.write(input);
+
+
+//převod vstupu do promptu na int
+let input = window.prompt("Zadej mi svůj věk",20);
+window.alert(`Za rok ti bude ${parseInt(input)+1}`);
+*/
+
+//Confirm - vrací true/false¨
+
+/*
+let input = window.confirm("Potvrzujete, že jste podváděl u testu?");
+if(input)
+{
+    document.write("Pak jste zvolil smrt!");
+} else 
+{
+    document.write("Gratuluji!");
+}
+*/
