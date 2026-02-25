4d87c6a1a152b9ba9fdd31faa872d09ff8cd2167
Jan Mottl <mottja@delta-skola.cz>
2026-02-25 08:39:48 +0100

    kk

---
+++ 2A/JS_DOM/pop_up_a/script.js
@@ -0,0 +1,31 @@
+//BOM pop-up
+
+//window BOM
+//document DOM
+
+//Alert - zpráva
+
+//window.alert("AAAAA!");
+
+//Prompt
+/*
+let jmeno = window.prompt("Zadej mi své jméno","Pepa");
+document.write("<h3>"+jmeno+"</h3>"); 
+*/
+/*
+let rok = window.prompt("Zadej letošní rok",2026);
+document.write(`Příští rok je ${parseInt(rok) + 1}`);
+*/
+
+//Confirm
+/*
+let potvrzeni = window.confirm("Potvrzuješ, že podvádíš u testů?"); //vrací true/false
+
+if(potvrzeni)
+{
+    document.write("Tak zhyň!");
+} else 
+{
+    document.write("Zvolil jsi moudře!");
+}
+*/
